1888e433601c232f484c6b6c2fd7867dce53f2ba
Eric Lok <ericlok@gmail.com>
2021-02-18 22:10:32 -0500

    added terraform files, screenshots, and updated policy

---
+++ Project 1 - Deploying a Web Server in Azure/terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "udacity-azure-webserver"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "East US"
+}
+
+  
+variable "resource_group" {
+  description = "Name of the resource group, including the -rg"
+  default     = "packer-rg"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of the tags to use for the resources that are deployed"
+  type        = map(string)
+  default = {
+    Name = "udacity-azure-webserver"
+  }
+}
+
+variable "username" {
+  description = "Enter username to associate with the machine"
+} 
+
+variable "password" {
+  description = "Enter password to use to access the machine"
+}
+
+variable "packer_resource_group" {
+  description = "Resource group of the Packer image"
+  default     =  "packer-rg"
+  type        = string
+}
+
+variable "packer_image_name" {
+  description = "Image name of the Packer image"
+  default     =  "webserverPackerImage"
+  type        = string
+}
+
+variable "num_of_vms" {
+  description = "Number of VM resources to be  created behnd the load balancer"
+  default     = 2
+  type        = number
+}
